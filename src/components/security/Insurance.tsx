ab926a7a97c703c396fe83a0b2e62e37b0e72624
Germán Martínez <germaga92@gmail.com>
2020-02-20 20:00:33 +0100

    Add insurance sction to security page

---
+++ src/components/security/Insurance.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+import { StaticQuery, graphql } from 'gatsby'
+import ContentWrapper from '../Layout/ContentWrapper'
+import ButtonLink from '../ui/ButtonLink'
+import LinesSVG from '../../assets/lines.svg'
+
+const Container = styled.div`
+  padding: 175px 0 328px 0;
+  background-color: ${p => p.theme.palette.greyBackground};
+  z-index: -1;
+  position: relative;
+  color: ${p => p.theme.palette.navy};
+`
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const LRow = styled.div`
+  margin-top: 50px;
+`
+
+const LCol = styled.p`
+  flex-basis: 25%;
+`
+
+const CCol = styled.p`
+  flex-basis: 35%;
+  padding-right: 30px;
+  letter-spacing: 0.36px;
+  line-height: normal;
+`
+
+const RCol = styled.p`
+  flex-basis: 35%;
+  font-size: 16px;
+  letter-spacing: 0.36px;
+  line-height: normal;
+`
+
+const SHeading = styled.h3`
+  font-size: 25px;
+  font-weight: 800;
+  letter-spacing: 0.56px;
+`
+
+const BoldText = styled.p`
+  font-weight: 800;
+  display: inline;
+`
+
+const SLinesSVG = styled(LinesSVG)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+`
+
+const Insurance = () => (
+  <Container>
+    <SLinesSVG />
+    <ContentWrapper>
+      <Row>
+        <LCol>
+          <SHeading>
+            Insurance
+            <br />
+            by Nexus Mutual
+          </SHeading>
+        </LCol>
+        <CCol>
+          <BoldText>
+            Still have doubts about the smart contracts’ security?
+          </BoldText>
+          You can insure funds stored in the Gnosis Safe against potential smart
+          contract attacks, through Nexus Mutual services.
+        </CCol>
+        <RCol>
+          Click below and use the smart contract address
+          gnosissafe.nexusmutual.eth to get cover!
+          <LRow>
+            <ButtonLink url="/" colorScheme="navy">
+              Get a quote
+            </ButtonLink>
+          </LRow>
+        </RCol>
+      </Row>
+    </ContentWrapper>
+  </Container>
+)
+
+export default Insurance
