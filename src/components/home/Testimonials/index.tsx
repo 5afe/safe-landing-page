0796c14ddb8ca368aaf20bb466e4ecd3f92f0629
Mikhail Mikheev <mmvsha73@gmail.com>
2020-02-24 12:06:50 +0100

    Testimonials/trustedby wip

--- src/components/home/Testimonials/index.tsx
+++ src/components/home/Testimonials/index.tsx
@@ -54,46 +54,62 @@ const Testimonials = () => (
     <ContentWrapper>
       <TestimonialsContainer>
         <SectionHeading>Testimonials</SectionHeading>
-        <Testimonial author="Jacques-Yves Cousteau" colorScheme="white">
-          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.”
+        <Testimonial
+          author="Jason Goldberg, Founder & CEO, Pepo/OST"
+          colorScheme="white"
+        >
+          The Pepo team decided to build on the Gnosis Safe stack because of its
+          modularity, flexibility, and security, allowing Pepo to take advantage
+          of session keys, multi-device management, and account recovery —
+          granting our users peace of mind while using a new social network.
         </Testimonial>
-        <Testimonial author="Jacques-Yves Cousteau" colorScheme="navy">
-          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.”
+        <Testimonial
+          author="Eric Conner, EthHub / Into The Ether Podcast"
+          colorScheme="navy"
+        >
+          We use the Gnosis Safe Multisig to manage EthHub's funds and love it.
+          The track record of Gnosis and their commitment to security is what
+          led us to it. It gives us peace of mind while running our crypto
+          business
         </Testimonial>
-        <Testimonial author="Jacques-Yves Cousteau">
-          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.”
+        <Testimonial author="Runtime Verification">
+          From October 2018 to February 2019, we were engaged in the formal
+          verification of the Gnosis Safe smart contract. We loved working with
+          Gnosis developers, and we’re very impressed by their commitment to
+          security in general, and formal verification in particular.
         </Testimonial>
-        <Testimonial author="Jacques-Yves Cousteau">
-          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.”
-        </Testimonial>
-        <Testimonial author="Jacques-Yves Cousteau" colorScheme="white">
-          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.”
-        </Testimonial>
-        <STestimonial1 author="Jacques-Yves Cousteau" colorScheme="white">
-          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim.”“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim.”“Lorem ipsum dolor sit amet,
+        <STestimonial1
+          author="Taylor Monahan, Founder & CEO, MyCrypto"
+          colorScheme="white"
+        >
+          The Gnosis Safe Multisig is the clear leader when it comes to a
+          product that balances usability, security, and self-custody. The
+          longevity of their smart contracts is evidence of the Gnosis team’s
+          comprehensive dedication to security, which is the most important
+          thing when choosing a place to store your crypto assets. It cannot be
+          matched
         </STestimonial1>
-        <STestimonial2 author="Jacques-Yves Cousteau" colorScheme="navy">
-          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim.”“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.”
+        <STestimonial2
+          author="Christophe Lassuyt, Co-Founder & CFO, Request Network"
+          colorScheme="navy"
+        >
+          The old Gnosis Multisig is a strong technology that lacked fluidity,
+          while the new Gnosis Safe Multisig is easy, modern and strong. The
+          team behind Gnosis is great technically, inspires confidence and is
+          composed of persons that we trust. We're making the transition to the
+          Safe Multisig over the next few months.
         </STestimonial2>
-        <STestimonial3 author="Jacques-Yves Cousteau" colorScheme="white">
-          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim.”“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim.”“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod
+        <STestimonial3
+          author="Manuel Garcia, CEO, Protofire"
+          colorScheme="white"
+        >
+          Protofire has been using the Gnosis Multisig wallet for years. We
+          recently migrated to the new Gnosis Safe Multisig which has become our
+          single interface for internal and external crypto-financial
+          activities. As Protofire provides professional services exclusively to
+          companies in the crypto space, most of our income arrives to the Safe
+          Mulltisig. That's how important the Gnosis Safe Multisig is to us and
+          how much we trust the technology and the team behind its development.
         </STestimonial3>
       </TestimonialsContainer>
     </ContentWrapper>
---
+++ src/components/home/TrustedBy/index.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
+import ContentWrapper from '../../Layout/ContentWrapper'
+
+const Container = styled.section`
+  padding-top: 50px;
+  padding-bottom: 315px;
+`
+
+const SWrapper = styled(ContentWrapper)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+`
+
+const LCol = styled.div`
+  flex-basis: 16%;
+  flex-direction: column;
+`
+
+const RCol = styled.div`
+  flex-basis: 65%;
+  width: 100%;
+`
+
+const SHeading = styled.h3`
+  font-size: 25px;
+  font-weight: 800;
+  letter-spacing: 0.56px;
+`
+
+const TrustedBy = () => (
+  <StaticQuery query={sectionQuery}>
+    {data => {
+      console.log(data)
+
+      return (
+        <Container>
+          <SWrapper>
+            <LCol>
+              <SHeading>Trusted by</SHeading>
+            </LCol>
+            <RCol></RCol>
+          </SWrapper>
+        </Container>
+      )
+    }}
+  </StaticQuery>
+)
+
+const sectionQuery = graphql`
+  query {
+    aragon: file(relativePath: { eq: "aragon.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    consensys: file(relativePath: { eq: "consensys.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    ethhub: file(relativePath: { eq: "ethhub.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    kyber: file(relativePath: { eq: "kyber.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    pepo: file(relativePath: { eq: "pepo.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    protofire: file(relativePath: { eq: "protofire.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    requestNet: file(relativePath: { eq: "requestnet.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    rv: file(relativePath: { eq: "rv.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    unilogin: file(relativePath: { eq: "unilogin.png" }) {
+      childImageSharp {
+        fixed(width: 268) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+  }
+`
+
+export default TrustedBy
