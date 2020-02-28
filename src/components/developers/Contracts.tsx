afb957c447065f679066788e00bc40bc301ef787
Germán Martínez <germaga92@gmail.com>
2020-02-28 15:06:45 +0100

    Responsive contracts section

--- src/components/developers/Contracts.tsx
+++ src/components/developers/Contracts.tsx
@@ -10,12 +10,19 @@ const Container = styled.div`
   padding-top: 112px;
   background-color: rgba(0, 140, 115, 0.88);
   overflow: hidden;
+  @media screen and (max-width: 1240px) {
+    padding: 20px 12px 30px 12px;
+  }
 `
 
 const Row = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 20px;
+  @media screen and (max-width: 1240px) {
+    display: block;
+    margin-bottom: 0;
+  }
 `
 
 const LCol = styled.div`
@@ -25,6 +32,9 @@ const LCol = styled.div`
 const RCol = styled.div`
   flex-basis: 72.5%;
   margin-bottom: 60px;
+  @media screen and (max-width: 1240px) {
+    margin-bottom: 0;
+  }
 `
 
 const SubLCol = styled.p`
@@ -32,6 +42,9 @@ const SubLCol = styled.p`
   letter-spacing: 0.36px;
   font-size: 16px;
   line-height: normal;
+  @media screen and (max-width: 1240px) {
+    padding-bottom: 30px;
+  }
 `
 
 const SubRCol = styled.p`
@@ -45,11 +58,19 @@ const SHeading = styled.h3`
   font-size: 25px;
   font-weight: 800;
   letter-spacing: 0.56px;
+  @media screen and (max-width: 1240px) {
+    padding: 30px 0;
+    font-size: 28px;
+  }
 `
 
 const SButtonLink = styled(ButtonLink)`
   width: 100%;
   text-align: center;
+  @media screen and (max-width: 1240px) {
+    width: 178px;
+    margin-bottom: 20px;
+  }
 `
 
 const SLinesSVG = styled(LinesSVG)`
