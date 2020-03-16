882effa14a42d4219d34fdb7348d9bdc2f24913e
Germán Martínez <germaga92@gmail.com>
2020-03-16 05:20:29 +0100

    Add projects section to enterprises page

---
+++ src/components/enterprises/Projects/index.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import styled from 'styled-components'
+import ContentWrapper from '../../Layout/ContentWrapper'
+import { PROJECTS } from './projects'
+
+const Container = styled.section`
+  padding-top: 50px;
+  padding-bottom: 230px;
+  color: ${p => p.theme.palette.grey};
+  background-color: ${p => p.theme.palette.navy};
+  overflow: hidden;
+  @media screen and (max-width: 1240px) {
+    padding: 0;
+  }
+`
+
+const SWrapper = styled(ContentWrapper)`
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  @media screen and (max-width: 1240px) {
+    display: block;
+    padding: 0px 10px 40px 10px;
+  }
+`
+
+const LCol = styled.div`
+  flex-basis: 25%;
+  flex-direction: column;
+`
+
+const RCol = styled.div`
+  flex-basis: 72.5%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 160px);
+  grid-column-gap: 91px;
+  grid-row-gap: 51px;
+  @media screen and (max-width: 1240px) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+  }
+`
+
+const SHeading = styled.h3`
+  font-size: 25px;
+  font-weight: 800;
+  letter-spacing: 0.56px;
+  line-height: normal;
+  @media screen and (max-width: 1240px) {
+    padding: 30px 0;
+    font-size: 28px;
+  }
+`
+
+const ImageAnchor = styled.a`
+  max-height: 54px;
+`
+
+const Image = styled.img`
+  max-width: 160px;
+  max-height: 54px;
+  width: auto;
+  height: auto;
+  border-radius: 50%;
+`
+
+const Projects = () => (
+  <Container>
+    <SWrapper>
+      <LCol>
+        <SHeading>Used by</SHeading>
+      </LCol>
+      <RCol>
+        {PROJECTS.map(project => (
+          <ImageAnchor
+            key={project.name}
+            title={project.name}
+            href={project.websiteURL}
+            target="_blank"
+          >
+            <Image src={project.imageURL}></Image>
+          </ImageAnchor>
+        ))}
+      </RCol>
+    </SWrapper>
+  </Container>
+)
+
+export default Projects
---
+++ src/components/enterprises/Projects/projects.ts
@@ -0,0 +1,15 @@
+interface Project {
+  name: string
+  imageURL: string
+  websiteURL: string
+}
+
+const PROJECTS: Project[] = [
+  {
+    name: '',
+    imageURL: '',
+    websiteURL: '',
+  },
+]
+
+export { PROJECTS }
