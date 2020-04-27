256216baca8ac4c25d5973ce73fc513a179f9e24
Germán Martínez <germaga92@gmail.com>
2020-04-28 00:07:23 +0200

    Integrate Intercom using safe-react DEVELOPMENT id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import Features from '../components/home/Features'
 import Testimonials from '../components/home/Testimonials'
 import MultisigMobile from '../components/home/MultisigMobile'
 import Social from '../components/home/Social'
+import { loadIntercom } from '../utils/intercom'
+
+loadIntercom()
 
 export default () => (
   <Layout title="Overview">
