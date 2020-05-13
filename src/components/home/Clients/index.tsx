9f17b64c493854a221316facf8a5e85ff9a9adba
Germán Martínez <germaga92@gmail.com>
2020-05-13 18:59:09 +0200

    Add links to download desktop app

--- src/components/home/Clients/index.tsx
+++ src/components/home/Clients/index.tsx
@@ -1,28 +1,24 @@
-import React, { /* useEffect,*/ useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DownloadButtons from './DownloadButtons'
 
 const Clients = () => {
-  const [desktopAppUrl, setDesktopAppUrl] = useState(
-    'https://github.com/gnosis/safe-react/releases'
-  )
+  const [desktopAppUrl, setDesktopAppUrl] = useState<string | undefined>(undefined)
 
-  /*
   // Uncomment when the safe-react files are available:
   useEffect(() => {
     if (navigator.appVersion.indexOf('Win') !== -1) {
-      setDesktopAppUrl('<WINDOWS_APP_URL>')
+      setDesktopAppUrl(process.env.GATSBY_URL_DOWNLOAD_WINDOWS)
     }
     if (navigator.appVersion.indexOf('Mac') !== -1) {
-      setDesktopAppUrl('<MAC_APP_URL>')
+      setDesktopAppUrl(process.env.GATSBY_URL_DOWNLOAD_MAC)
     }
     if (navigator.appVersion.indexOf('X11') !== -1) {
-      setDesktopAppUrl('<UNIX_APP_URL>')
+      setDesktopAppUrl(process.env.GATSBY_URL_DOWNLOAD_LINUX)
     }
     if (navigator.appVersion.indexOf('Linux') !== -1) {
-      setDesktopAppUrl('<LINUX_APP_URL>')
+      setDesktopAppUrl(process.env.GATSBY_URL_DOWNLOAD_LINUX)
     }
   }, [])
-  */
 
   return <DownloadButtons desktopAppUrl={desktopAppUrl} />
 }
