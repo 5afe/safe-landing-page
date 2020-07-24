3939c208ee70af887e8552de66c4b68153fae843
Germán Martínez <germaga92@gmail.com>
2020-07-24 16:11:34 +0200

    Update features in overview page

--- src/components/home/Features/GetNotified.tsx
+++ src/components/home/Features/GetNotified.tsx
@@ -3,50 +3,46 @@ import styled from 'styled-components'
 import addToMailchimp from 'gatsby-plugin-mailchimp'
 import Button from '../../ui/Button'
 import Input from '../../ui/Input'
+import LinkIcon from '../../../assets/link.svg'
 
-const Row = styled.div`
-  display: flex;
-  justify-content: space-between;
+const SHeading = styled.h3`
   width: 100%;
+  text-align: center;
+  font-size: 44px;
+  line-height: 1.18;
+  padding-top: 50px;
+  font-weight: 800;
+  letter-spacing: 0.56px;
+  @media screen and (max-width: 1240px) {
+    padding: 30px 0;
+    font-size: 28px;
+  }
+`
+
+const LetUsKnow = styled.div`
+  font-size: 24px;
+  text-align: center;
+  line-height: 1.25;
+  padding: 20px 0;
+`
+
+const SForm = styled.form`
+  display: flex;
+  padding: 45px 0 100px 0;
+  justify-content: center;
   @media screen and (max-width: 1240px) {
     display: block;
   }
 `
 
 const Col = styled.div`
-  flex-basis: 48%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`
-
-const LCol = styled.div`
-  flex-basis: 25%;
-  flex-direction: column;
-`
-
-const RCol = styled.div`
-  flex-basis: 72.5%;
-`
-
-const SHeading = styled.h3`
-  font-size: 25px;
-  font-weight: 800;
-  letter-spacing: 0.56px;
-  margin-bottom: 29px;
-`
-
-const SForm = styled.form`
-  display: flex;
+  width: 420px;
   position: relative;
-  @media screen and (max-width: 1240px) {
-    display: block;
-  }
+  margin-right: 20px;
 `
-
 const SInput = styled(Input)`
+  height: 52px;
   width: 100%;
-  margin-right: 50px;
   @media screen and (max-width: 1240px) {
     max-width: 440px;
     margin-right: 0;
@@ -58,12 +54,27 @@ const SMsg = styled.p`
 `
 
 const SButton = styled(Button)`
-  max-width: 100px;
+  width: 180px;
+  height: 52px;
+  font-size: 16px;
+  outline: none;
+
   @media screen and (max-width: 1240px) {
     max-width: 205px;
     margin: 40px 0;
     margin-right: 0;
   }
+`
+
+const SLinkIcon = styled(LinkIcon)`
+  margin-left: 5px;
+`
+
+const InnerLink = styled.div`
+  display: inline-flex;
+  align-items: center;
+  color: ${(p) => p.theme.palette.primary};
+  text-decoration: underline;
 `
 
 const GetNotified = () => {
@@ -94,37 +105,38 @@ const GetNotified = () => {
   }
 
   return (
-    <Row>
-      <LCol>
-        <SHeading>
-          Get notified about
-          <br />
-          future releases
-        </SHeading>
-      </LCol>
-      <RCol>
-        <SForm onSubmit={handleSubmit}>
-          <Row>
-            <Col>
-              <SInput
-                type="email"
-                placeholder="Your Email Address"
-                value={email}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setEmail(e.target.value)
-                }}
-              />
-              {msg && <SMsg>{msg}</SMsg>}
-            </Col>
-            <Col>
-              <SButton colorScheme="white" type="submit">
-                Sign up
-              </SButton>
-            </Col>
-          </Row>
-        </SForm>
-      </RCol>
-    </Row>
+    <>
+      <SHeading>Missing a feature?</SHeading>
+      <LetUsKnow>
+        <span>Let us know!{' '}</span>
+        <a
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InnerLink>
+            Feature idea board<SLinkIcon />
+          </InnerLink>
+        </a>
+      </LetUsKnow>
+      <SHeading>Get notified about future releases</SHeading>
+      <SForm onSubmit={handleSubmit}>
+        <Col>
+          <SInput
+            type="email"
+            placeholder="Your e-mail address"
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.target.value)
+            }}
+          />
+          {msg && <SMsg>{msg}</SMsg>}
+        </Col>
+        <SButton colorScheme="green" type="submit">
+          Sign up
+        </SButton>
+      </SForm>
+    </>
   )
 }
 
--- src/components/home/Features/index.tsx
+++ src/components/home/Features/index.tsx
@@ -1,28 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-import Feature from './Feature'
-import GetNotified from './GetNotified'
+import Img from 'gatsby-image'
+import { StaticQuery, graphql } from 'gatsby'
 import ContentWrapper from '../../Layout/ContentWrapper'
-import ButtonLink from '../../ui/ButtonLink'
-import Link from '../../ui/Link'
-import DocIcon from '../../../assets/icon-doc.svg'
-import GroupIcon from '../../../assets/icon-group.svg'
-import SafeIcon from '../../../assets/icon-safe.svg'
-import WalletIcon from '../../../assets/icon-wallet.svg'
-import PersonIcon from '../../../assets/icon-person.svg'
-import WarningIcon from '../../../assets/icon-warning.svg'
-
-const SFeature = styled(Feature)`
-  margin-bottom: 47px;
-  position: relative;
-  @media screen and (max-width: 1240px) {
-    margin: 0 0 30px 0;
-  }
-`
+import GetNotified from './GetNotified'
 
 const Container = styled.section`
-  padding: 112px 0;
-  background-color: rgba(0, 140, 115, 0.88);
   overflow: hidden;
   color: ${(p) => p.theme.palette.navy};
   @media screen and (max-width: 1240px) {
@@ -30,112 +13,156 @@ const Container = styled.section`
   }
 `
 
-const Row = styled.div`
-  display: flex;
-  justify-content: space-between;
+const SWrapper = styled(ContentWrapper)`
   height: 100%;
-  margin-bottom: 80px;
   @media screen and (max-width: 1240px) {
     display: block;
-    margin-bottom: 40px;
+    padding: 0px 10px 40px 10px;
   }
-`
-
-const LCol = styled.div`
-  flex-basis: 25%;
-  flex-direction: column;
-`
-
-const Col = styled.div`
-  flex-basis: 35%;
 `
 
 const SHeading = styled.h3`
-  font-size: 25px;
+  width: 100%;
+  text-align: center;
+  font-size: 44px;
+  line-height: 1.18;
+  padding-bottom: 50px;
   font-weight: 800;
   letter-spacing: 0.56px;
-  margin-bottom: 29px;
   @media screen and (max-width: 1240px) {
-    margin: 30px 0;
+    padding: 30px 0;
     font-size: 28px;
   }
 `
 
-const Text = styled.p`
-  letter-spacing: 0.36px;
-  margin-bottom: 37px;
-  line-height: normal;
-  @media screen and (max-width: 1240px) {
-    letter-spacing: normal;
-    margin-bottom: 30px;
-    font-size: 28px;
-    font-weight: 800;
-  }
+interface BlockProps {
+  align?: string
+}
+
+const Block = styled.div`
+  text-align: ${(p: BlockProps) => p.align};
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 100px;
 `
 
-const SButtonLink = styled(ButtonLink)`
-  margin-bottom: 40px;
+const Text = styled.div`
+  max-width: 400px;
 `
 
-const SLink = styled(Link)`
-  text-decoration: underline;
-  font-weight: bold;
+const Title = styled.h3`
+  font-size: 32px;
+  font-weight: 800;
+  line-height: 1.13;
+  padding-top: 80px;
+`
+
+const Description = styled.h5`
+  font-size: 20px;
+  line-height: 1.3;
+`
+
+const Image = styled.div`
+  width: 760px;
 `
 
 const Features = () => (
-  <Container>
-    <ContentWrapper>
-      <Row>
-        <LCol>
+  <StaticQuery
+    query={graphql`
+      query {
+        apps: file(relativePath: { eq: "apps.png" }) {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        assets: file(relativePath: { eq: "assets.png" }) {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        multisig: file(relativePath: { eq: "multisig.png" }) {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        wallets: file(relativePath: { eq: "wallets.png" }) {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+  >
+    {(data) => (
+      <Container>
+        <SWrapper>
           <SHeading>Features</SHeading>
-          <Text>
-            Missing a feature?
-            <br />
-            Let us know!
-          </Text>
-          <SButtonLink url="http://safe.cnflx.io/" colorScheme="white">
-            Request a feature
-          </SButtonLink>
-        </LCol>
-        <Col>
-          <SFeature img={DocIcon} title="Multi-signature">
-            Fully customize how you manage your digital assets, with the option
-            to require multiple signatures to confirm transactions for
-            additional security.
-          </SFeature>
-          <SFeature img={SafeIcon} title="Security-first">
-            The Safe Multisig is the only formally verified and audited smart
-            contract wallet.{' '}
-            <SLink to="/security">
-              Learn more about how we put security first
-            </SLink>
-            .
-          </SFeature>
-          <SFeature img={GroupIcon} title="Gasless Signatures">
-            Enhanced efficiency and user experience with gasless signatures.
-            Confirm or reject transactions instantly with no fee required.
-          </SFeature>
-        </Col>
-        <Col>
-          <SFeature img={WalletIcon} title="Wallet-agnostic">
-            Use your favorite wallet to sign transactions. A wide range of
-            support for hardware, browser or mobile wallets gives maximum
-            flexibility.
-          </SFeature>
-          <SFeature img={PersonIcon} title="Non-custodial">
-            You are always in charge of your digital assets. As a self-sovereign
-            wallet solution, no one is able to access or freeze any of your
-            funds.
-          </SFeature>
-          <SFeature img={WarningIcon} title="Privacy">
-            Information, such as owner names, is only stored locally. This means
-            that Gnosis or any third-party can never access your private data.
-          </SFeature>
-        </Col>
-      </Row>
-      <GetNotified />
-    </ContentWrapper>
-  </Container>
+          <Block align="left">
+            <Text>
+              <Title>Multi-signature</Title>
+              <Description>
+                Fully customize how you manage your company crypto assets, with
+                the option to require a predefined number of signatures to
+                confirm transactions. Require multiple team members to confirm
+                every transaction in order to execute it, which helps prevent
+                unauthorized access to company crypto.
+              </Description>
+            </Text>
+            <Image>
+              <Img fluid={data.multisig.childImageSharp.fluid} />
+            </Image>
+          </Block>
+          <Block align="right">
+            <Image>
+              <Img fluid={data.assets.childImageSharp.fluid} />
+            </Image>
+            <Text>
+              <Title>Assets</Title>
+              <Description>
+                Gnosis Safe Multisig supports ETH,<br/> ERC20 (Tokens) and ERC721<br/>
+                (Collectibles). You can also see the fiat values of your assets.
+              </Description>
+            </Text>
+          </Block>
+          <Block align="left">
+            <Text>
+              <Title>Use any wallet</Title>
+              <Description>
+                Use your favorite wallet to sign transactions. No matter if it’s
+                a mobile wallet, browser extension, or hardware wallet.
+              </Description>
+            </Text>
+            <Image>
+              <Img fluid={data.wallets.childImageSharp.fluid} />
+            </Image>
+          </Block>
+          <Block align="right">
+            <Image>
+              <Img fluid={data.apps.childImageSharp.fluid} />
+            </Image>
+            <Text>
+              <Title>Safe Apps </Title>
+              <Description>
+                Bringing multisig security to DeFi, you can now put your funds
+                to work directly from the Gnosis Safe Multisig interface. Use
+                your digital assets to invest, earn, borrow, invoice, do payroll
+                and more.
+              </Description>
+            </Text>
+          </Block>
+          <GetNotified />
+        </SWrapper>
+      </Container>
+    )}
+  </StaticQuery>
 )
 
 export default Features
