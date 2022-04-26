6e9d2579ecad6644ba78bf8a21f56fc6c368d097
Usame Algan <usame.algan@gnosis.pm>
2022-04-26 18:13:09 +0200

    feat: Add Open positions section for career page

---
+++ src/components/careers/Positions/index.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import { Grid } from '@material-ui/core'
+import styled from 'styled-components'
+
+import { Card } from './Card'
+import ContentWrapper from '../../Layout/ContentWrapper'
+
+const JOBS_API_URL = 'https://boards-api.greenhouse.io/v1/boards/gnosis/jobs'
+
+const SectionTitle = styled.h2`
+  font-size: 45px;
+  font-weight: bolder;
+  text-align: center;
+  margin-bottom: 56px;
+`
+
+type JobMeta = {
+  id: number
+  name: string
+  value: unknown
+  value_type: string
+}
+
+type Job = {
+  absolute_url: string
+  data_compliance: unknown[]
+  id: number
+  internal_job_id: number
+  location: {
+    name: string
+  }
+  metadata: JobMeta[]
+  requisition_id: unknown
+  title: string
+  updated_at: string
+}
+
+type Jobs = {
+  jobs: Job[]
+  meta: {
+    total: number
+  }
+}
+
+export const Positions = () => {
+  const [positions, setPositions] = useState<Jobs>()
+
+  useEffect(() => {
+    let isCurrent = true
+
+    const getOpenPositions = async () => {
+      try {
+        const data = await fetch(JOBS_API_URL)
+        const jobs: Jobs = await data.json()
+
+        if (isCurrent) {
+          setPositions(jobs)
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    getOpenPositions()
+
+    return () => {
+      isCurrent = false
+    }
+  }, [])
+
+  return (
+    <ContentWrapper>
+      <SectionTitle>Open positions</SectionTitle>
+      <Grid container spacing={3}>
+        {positions &&
+          positions.jobs.map((job) => {
+            return (
+              <Grid key={job.id} item xs={12} md={4}>
+                <Card
+                  location={job.location.name}
+                  title={job.title}
+                  url={job.absolute_url}
+                />
+              </Grid>
+            )
+          })}
+      </Grid>
+    </ContentWrapper>
+  )
+}
--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -3,11 +3,13 @@ import * as React from 'react'
 import Layout from '../components/Layout'
 import Benefits from '../components/careers/Benefits'
 import Leadership from '../components/careers/Leadership'
+import {Positions} from "../components/careers/Positions";
 
 export default () => (
   <Layout title="Careers">
     <Benefits />
     <Leadership />
+    <Positions />
     <Locations />
   </Layout>
 )
