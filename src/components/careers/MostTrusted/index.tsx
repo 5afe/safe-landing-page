ee4c2e126db6b91485dd10fcd60a1ef8ad30e4d0
Usame Algan <usame.algan@gnosis.pm>
2022-04-27 14:31:14 +0200

    style: Image positioning

--- src/components/careers/MostTrusted/index.tsx
+++ src/components/careers/MostTrusted/index.tsx
@@ -1,26 +1,15 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import ContentWrapper from '../../Layout/ContentWrapper'
+import { Grid } from '@material-ui/core'
 
 const Section = styled.section`
   width: 100%;
-  height: 100vh;
+  padding: 170px 0;
   background-color: ${(p) => p.theme.palette.white};
 
   @media screen and (max-width: 768px) {
     height: auto;
-  }
-`
-
-const Content = styled.div`
-  width: 542px;
-  margin-top: 180px;
-  margin-left: 200px;
-
-  @media screen and (max-width: 768px) {
-    margin-top: 64px;
-    margin-left: 0;
-    width: 100%;
-    padding: 0 16px;
   }
 `
 
@@ -38,7 +27,6 @@ const Title = styled.p`
 `
 
 const Text = styled.p`
-  line-height: normal;
   flex: 1;
   font-size: 24px;
   line-height: 32px;
@@ -52,7 +40,6 @@ const Text = styled.p`
 `
 
 const SecondaryText = styled.p`
-  line-height: normal;
   flex: 1;
   font-size: 16px;
   line-height: 24px;
@@ -60,20 +47,11 @@ const SecondaryText = styled.p`
 
 const Image = styled.img`
   height: 360px;
-  margin-left: 60px;
-  margin-top: 160px;
-  object-fit: cover;
-  object-position: right;
+  position: absolute;
+  right: 0;
 
-  @media screen and (max-width: 768px) {
-    margin: 40px 0;
-    padding-right: 0px;
-    margin-left: 16px;
-    height: 300px;
-
-    object-fit: cover;
-    object-position: left;
-    width: 100%;
+  @media screen and (max-width: 960px) {
+    position: relative;
   }
 `
 
@@ -85,6 +63,7 @@ const Container = styled.div`
 
   @media screen and (max-width: 768px) {
     flex-direction: column;
+    align-items: flex-end;
   }
 `
 
@@ -92,19 +71,23 @@ const MostTrusted = () => {
   return (
     <Section>
       <Container>
-        <Content>
-          <Title>The most trusted wallet on Ethereum</Title>
-          <Text>
-            Gnosis Safe is the most trusted platform to manage digital assets on
-            Ethereum and other leading protocols.
-          </Text>
-          <SecondaryText>
-            Believing in the power of decentralisation and open-source software
-            development, we are building the Safe web and mobile application,
-            developer tools and an inclusive online community to foster the core
-            building blocks of the Web3 ecosystem.
-          </SecondaryText>
-        </Content>
+        <ContentWrapper>
+          <Grid container style={{ position: 'relative' }}>
+            <Grid item xs={12} md={6}>
+              <Title>The most trusted wallet on Ethereum</Title>
+              <Text>
+                Gnosis Safe is the most trusted platform to manage digital
+                assets on Ethereum and other leading protocols.
+              </Text>
+              <SecondaryText>
+                Believing in the power of decentralisation and open-source
+                software development, we are building the Safe web and mobile
+                application, developer tools and an inclusive online community
+                to foster the core building blocks of the Web3 ecosystem.
+              </SecondaryText>
+            </Grid>
+          </Grid>
+        </ContentWrapper>
         <Image src="/images/Illustration-wallet.png" />
       </Container>
     </Section>
