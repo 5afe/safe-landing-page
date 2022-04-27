054f76c14c037bc4f807474b50870bb2111f3edc
Usame Algan <usame.algan@gnosis.pm>
2022-04-27 13:53:53 +0200

    feat: Add initiative application section

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -5,12 +5,14 @@ import Benefits from '../components/careers/Benefits'
 import Leadership from '../components/careers/Leadership'
 import Positions from '../components/careers/Positions'
 import Locations from '../components/careers/Locations'
+import CallToAction from '../components/careers/CTA'
 
 export default () => (
   <Layout title="Careers">
     <Benefits />
     <Leadership />
     <Positions />
+    <CallToAction />
     <Locations />
   </Layout>
 )
