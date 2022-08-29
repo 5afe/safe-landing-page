1641c71beb318fabdee3810883e65b915593c7f1
Aaron Cook <aaron.cook@gnosis.pm>
2022-08-29 14:46:07 +0200

    fix: wrong link (#69)
    * fix: wrong link
    
    * fix: wrong link

--- src/components/security/Main.tsx
+++ src/components/security/Main.tsx
@@ -169,7 +169,7 @@ const MainSection = () => {
             </a>
           </UsdWorthDate>
           <SButtonLink
-            url="https://explore.duneanalytics.com/dashboard/gnosis-safe_2"
+            url="https://dune.com/safe/ethereum"
             target="_blank"
             explicitExternal
           >
