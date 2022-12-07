7a541d81e79935b4fc9873da23c2e5d4696608bc
Usame Algan <usame@safe.global>
2022-12-07 09:31:38 +0100

    fix: Include gnosis-safe-io in prod tracking, adujst GA naming

--- src/utils/googleAnalytics.ts
+++ src/utils/googleAnalytics.ts
@@ -9,10 +9,11 @@ export const DEVELOPERS_CATEGORY = 'DEVELOPERS'
 export const FOOTER_CATEGORY = 'FOOTER_CATEGORY'
 
 const getGoogleAnalyticsTrackingID = () => {
-  return typeof window !== 'undefined' &&
-    window.location.hostname.split('.')[0] === 'safe'
-    ? process.env.GATSBY_GA_MAINNET
-    : process.env.GATSBY_GA_RINKEBY
+  return (typeof window !== 'undefined' &&
+    window.location.hostname.split('.')[0] === 'safe') ||
+    window.location.hostname.split('.')[0] === 'gnosis-safe'
+    ? process.env.GATSBY_GA_PROD
+    : process.env.GATSBY_GA_STAGING
 }
 
 let analyticsLoaded = false
